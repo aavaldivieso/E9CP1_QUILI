7dc781c5e0ec4c35a2cbd80f558d31cceea81901
Andres Valdivieso <aavaldivieso@uc.cl>
2019-01-07 21:59:52 -0300

    ejercicio 2 terminado

---
+++ 02/script.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('article').eq(2).find('a').eq(2).html('Pinterest').attr('href', 'https://pinterest.com')
+	$('a[href="#"]').click(function(evento) {
+		evento.preventDefault();
+	})
+	$('.header__main').find('h2').css('font-size', '4em')
+	$
+})
+
