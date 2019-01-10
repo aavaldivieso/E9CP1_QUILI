b7edd6122670b6ab548cac29b754a4b817f7815d
Andres Valdivieso <aavaldivieso@uc.cl>
2019-01-09 21:11:01 -0300

    todos los ejercicios terminados

--- 04/script.js
+++ 04/script.js
@@ -1,9 +1,39 @@
 $(function(){
+
+
 	$('.add_name').click(function(event){
+
 		var bariavle = prompt('Por favor agrega un nombre a la lista', 'Harry Potter')
-		$('.names').append('<li><span>' + bariavle + '</span> <a href="#" class="remove.name">Borrar</a></li>')
+		$('.names').append('<li><span>' + bariavle + '</span> <a href="#" class="remove_name">Borrar</a></li>')
 	})
-	$('.remove_name').click(function(evento) {
+
+
+	$('body').on('click', '.remove_name', function(evento){
 		evento.preventDefault();
+		$(this).parent().remove();
+		
 	})
+
 })
+
+//var lista = 0;
+
+//$(function(){
+
+
+	//$('.add_name').click(function(event){
+		//lista = lista+1;
+
+		//var bariavle = prompt('Por favor agrega un nombre a la lista', 'Harry Potter ')
+		//$('.names').append('<li><span>' + bariavle + '(' + lista + ')</span> <a href="#" class="remove_name">Borrar</a></li>')
+	//})
+
+
+	//$('body').on('click', '.remove_name', function(evento){
+		//evento.preventDefault();
+		//$(this).parent().remove();
+		//alert('me estoy eliminando');
+		
+	//})
+
+//})
